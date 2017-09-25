88bf0e73eb6ab258db4d3654c2f5fc52da5ae1c2
guosenlin1026 <445931644@qq.com>
2017-09-25 18:34:16 +0800

    编辑seed

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -19,6 +19,14 @@ class UserTableSeeder extends Seeder
             $model->email = 'user@qq.com';
             $model->phone = 15266668888;
             $model->save();
+
+            $model = new User;
+            $model->name = 'admin';
+            $model->password = bcrypt('admin');
+            $model->email = 'admin@qq.com';
+            $model->phone = 13666666666;
+            $model->type = 1;
+            $model->save();
         }
     }
 }
