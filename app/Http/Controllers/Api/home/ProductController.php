63361c034e94144607354210d523aade4f4df59d
guosenlin1026 <445931644@qq.com>
2017-09-29 17:23:26 +0800

    列表接口添加搜索接口

--- app/Http/Controllers/Api/home/ProductController.php
+++ app/Http/Controllers/Api/home/ProductController.php
@@ -84,24 +84,30 @@ class ProductController extends Controller
 		$cid = $request->category_id; // 分类id
 		$lon = $request->lon;
 		$lat = $request->lat;
+		$name = $request->name;
 
 		if (empty($type)) { //普通分类
 			$data = $this->getCategoryProduct($cid, $lon, $lat);
 		} else if ($type == 'recommend') { //推荐
 			$data = $this->getCategoryProduct($cid, $lon, $lat);
-		} else if ($type == 'nearby'){ //附近
+		} else if ($type == 'nearby') { //附近
 			$data = $this->getCategoryProduct(false, $lon, $lat);
+		} else if ($type == 'search') { //搜索
+			$data = $this->getCategoryProduct($cid, $lon, $lat, $name);
 		}
 		return response()->json($data);
 	}
 
 	//获取普通分类的列表产品/附近
-	public function getCategoryProduct($cid, $lon, $lat)
+	public function getCategoryProduct($cid, $lon, $lat, $name='')
 	{
 		$distance = ACOS(SIN(($lat * 3.1415) / 180 ) * SIN(('weft' * 3.1415) / 180 ) +COS(($lat * 3.1415) / 180 ) * COS(('weft' * 3.1415) / 180 ) * COS(($lon * 3.1415) / 180 - ('meridian' * 3.1415) / 180 ) ) * 6380;
 		$data = new Product;
 		if ($cid) {
 			$data = $data->where('category_id', $cid);
+		}
+		if ($name != '') {
+			$data = $data->where('products.name','like','%'.$name.'%');
 		}
 		$data = $data->select(DB::raw($distance.' as dis, products.*'))
 			->orderBy('dis', 'desc')
