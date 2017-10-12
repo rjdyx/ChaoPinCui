addc27102ef58ff9dcf88d3f9e10c23c017e17ca
limingcun <1521010816@qq.com>
2017-10-12 09:49:55 +0800

    [MOD]修改ProductController.php

--- app/Http/Controllers/Api/home/ProductController.php
+++ app/Http/Controllers/Api/home/ProductController.php
@@ -44,15 +44,14 @@ class ProductController extends Controller
 	// 获取产品评论信息
 	public function productComment($id)
 	{
-		$data = Comment::join('users','comments.user_id','=','users.id')
-			->join('products','comments.product_id','=','products.id')
+		$data = Comment::join('users','comments.user_id','=','users.id')->whereNull('users.deleted_at')
+			->join('products','comments.product_id','=','products.id')->whereNull('products.deleted_at')
 			->where('comments.product_id', $id)
 			->orderBy('created_at','desc')
 			->select('comments.*','users.img as user_img', 'users.name as user_name','products.comment as count_comment')
 			->get();
 		foreach ($data as $key => $value) {
 			$data[$key]->img = explode(',', $value->img);
-			$data[$key]->thumb = explode(',', $value->thumb);
 		}
 		return $data;
 	}
