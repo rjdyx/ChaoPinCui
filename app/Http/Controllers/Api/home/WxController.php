b3a33dfd2f7aa1a173a7195978035e1ee9e26429
limingcun <1521010816@qq.com>
2017-11-08 17:19:45 +0800

    gfds

--- app/Http/Controllers/Api/home/WxController.php
+++ app/Http/Controllers/Api/home/WxController.php
@@ -159,7 +159,7 @@ class WxController extends Controller
     // 微信直接登陆
     public function wxLogin(Request $request) {
         $openid = $request->openid;
-        $user = User::where('openid','=',$openid)->first();
+        $user = User::where('openid','=','oFDbs0H6qVByMqboE9tc7uo9agPQ')->first();
         return $user;
         if ($user != null) {
             $this->guard()->login($user);
