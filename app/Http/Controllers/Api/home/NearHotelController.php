42bc00f8b881c099444aa7ca36cefee6f660447d
limingcun <1521010816@qq.com>
2017-11-27 17:39:06 +0800

    [MOD]修改NearHotelController.php

--- app/Http/Controllers/Api/home/NearHotelController.php
+++ app/Http/Controllers/Api/home/NearHotelController.php
@@ -15,7 +15,7 @@ class NearHotelController extends Controller
 		$round = 1000;
 		$weft = $request->weft;  //纬度
 		$meridian = $request->meridian;  //经度
-		$url = "http://apis.map.qq.com/ws/place/v1/search?keyword=%E9%85%92%E5%BA%97&boundary=nearby($weft,$meridian,$round)&orderby=_distance&key=5ZZBZ-5Q363-U3K3I-YDD5M-R4EAO-NBBG4&page_index=";
+		$url = "http://apis.map.qq.com/ws/place/v1/search?keyword=%E9%85%92%E5%BA%97&boundary=nearby($weft,$meridian,$round)&orderby=_distance&key=5ZZBZ-5Q363-U3K3I-YDD5M-R4EAO-NBBG4&page_index=$page";
 		$infos = file_get_contents($url);
 		// $infos = json_decode($infos, true);
 		return $infos;
