d5237558dc8eba71f44ced57d1c08b78ae7b131e
limingcun <1521010816@qq.com>
2017-11-28 11:59:56 +0800

    [MOD]修改NearHotelController.php

--- app/Http/Controllers/Api/home/NearHotelController.php
+++ app/Http/Controllers/Api/home/NearHotelController.php
@@ -15,9 +15,12 @@ class NearHotelController extends Controller
 		$round = 1000;
 		$weft = $request->weft;  //纬度
 		$meridian = $request->meridian;  //经度
-		$url = "http://apis.map.qq.com/ws/place/v1/search?keyword=%E9%85%92%E5%BA%97&boundary=nearby($weft,$meridian,$round)&orderby=_distance&key=5ZZBZ-5Q363-U3K3I-YDD5M-R4EAO-NBBG4&page_index=$page";
-		$infos = file_get_contents($url);
-		// $infos = json_decode($infos, true);
+		$infos = IQuery::redisGet('near_hotel_info_'.$weft.$meridian.$page);
+		if (!isset($infos)) {
+			$url = "http://apis.map.qq.com/ws/place/v1/search?keyword=%E9%85%92%E5%BA%97&boundary=nearby($weft,$meridian,$round)&orderby=_distance&key=5ZZBZ-5Q363-U3K3I-YDD5M-R4EAO-NBBG4&page_index=$page";
+			$infos = file_get_contents($url);
+			IQuery::redisSet('near_hotel_info_'.$weft.$meridian.$page, $infos);
+		}
 		return $infos;
 	}
 }
