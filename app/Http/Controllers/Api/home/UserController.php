efdd8b0521d81336268f040664c7bb62f5ef335a
limingcun <1521010816@qq.com>
2017-12-04 15:23:37 +0800

    [MOD]修改UserController.php

--- app/Http/Controllers/Api/home/UserController.php
+++ app/Http/Controllers/Api/home/UserController.php
@@ -42,13 +42,12 @@ class UserController extends Controller
         if ($this->unquired($request,'name', $id)) return 101;
         if ($this->unquired($request,'email', $id)) return 102;
         if ($this->unquired($request,'phone', $id)) return 103;
-        $arr = ['name','real_name','sex','age','email','phone','address','img'];
+        $arr = ['name','real_name','sex','age','email','phone','address'];
         $model->setRawAttributes($request->only($arr));
         if ($type!=1) {
             $model->type = 0;
         }
         if ($request->password) $model->password = bcrypt($request->password);
-        $model->img = IQuery::singleImg($request,'img');
         if ($model->save()) return $id;
         return 0;
     }
